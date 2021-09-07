3262111c32471f55688e617781669f5ffaa5dabd
walshak <walshak1999@gmail.com>
2021-09-07 16:35:46 +0100

    updated footer and about sections to reflect Gloftech in auth layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,7 @@
         <!-- footer -->
         <!-- ============================================================== -->
         <footer class="footer text-center"> {{ date('Y') }} © Cashplug africa brought to you by <a
-                href="https://www.cashplug.com/">Cashplug.com</a>
+                href="https://www.cashplug.com/">Cashplug.com</a> Powered by Gloftech Nig. LTD
         </footer>
         <!-- ============================================================== -->
         <!-- End footer -->
--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -89,7 +89,7 @@ background-size: cover;">
         <!-- footer -->
         <!-- ============================================================== -->
         <footer class="footer text-center"> {{ date('Y') }} © Cashplug <a
-                href="https://www.cashplug.com/">cashplug.com</a>
+                href="https://www.cashplug.com/">cashplug.com</a> Powered by Gloftech Nig. LTD
         </footer>
         <!-- ============================================================== -->
         <!-- End footer -->
